4f4aef5611204baa9c454c541b12161e82733983
Eliecer Hernandez <eliecerhdz@gmail.com>
2016-05-31 18:08:11 -0500

    Fix EmployeeDirectoryServiceImpl functions

--- src/main/java/com/twilio/employeedirectory/domain/service/EmployeeDirectoryServiceImpl.java
+++ src/main/java/com/twilio/employeedirectory/domain/service/EmployeeDirectoryServiceImpl.java
@@ -11,6 +11,7 @@ import org.apache.http.NameValuePair;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.swing.text.html.Option;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -21,69 +22,72 @@ import java.util.Optional;
 @Singleton
 public class EmployeeDirectoryServiceImpl implements EmployeeDirectoryService {
 
-    private final EmployeeRepository repository;
+  private final EmployeeRepository repository;
 
-    @Inject
-    public EmployeeDirectoryServiceImpl(EmployeeRepository repository) {
-        this.repository = repository;
-    }
+  @Inject
+  public EmployeeDirectoryServiceImpl(EmployeeRepository repository) {
+    this.repository = repository;
+  }
 
-    @Override
-    public EmployeeMatch queryEmployee(String fullName, Optional<List<NameValuePair>> lastQuery) {
-        Optional<Employee> requestedEmployee = getRequestedEmployee(fullName, lastQuery);
-        List<Employee> matchedEmployees = requestedEmployee.map(employee -> {
-            List employees = new LinkedList<>();
-            employees.add(requestedEmployee.get());
-            return employees;
-        }).orElse(repository.findEmployeeByFullName(fullName));
-        return convertToEmployeeMatch(matchedEmployees);
-    }
+  @Override
+  public EmployeeMatch queryEmployee(String fullName, Optional<List<NameValuePair>> lastQuery) {
+    Optional<Employee> requestedEmployee = getRequestedEmployee(fullName, lastQuery);
+    List<Employee> matchedEmployees = requestedEmployee.map(employee -> {
+      List employees = new LinkedList<>();
+      employees.add(requestedEmployee.get());
+      return employees;
+    }).orElse(repository.findEmployeeByFullName(fullName));
+    return convertToEmployeeMatch(matchedEmployees);
+  }
 
-    /**
-     * Converts found Employees to an implementation of {@link EmployeeMatch}
-     *
-     * @param foundEmployees A List of {@link Employee}
-     * @return An {@link EmployeeMatch} indicating a particular type of result, not <code>null</code>
-     */
-    protected EmployeeMatch convertToEmployeeMatch(List<Employee> foundEmployees) {
-        final EmployeeMatch response;
-        switch (foundEmployees.size()) {
-            case 0:
-                response = new NoMatch();
-                break;
-            case 1:
-                response = new PerfectMatch(foundEmployees.get(0));
-                break;
-            default:
-                response = new MultiplePartialMatch(foundEmployees);
-        }
-        return response;
+  /**
+   * Converts found Employees to an implementation of {@link EmployeeMatch}
+   *
+   * @param foundEmployees A List of {@link Employee}
+   * @return An {@link EmployeeMatch} indicating a particular type of result, not <code>null</code>
+   */
+  protected EmployeeMatch convertToEmployeeMatch(List<Employee> foundEmployees) {
+    final EmployeeMatch response;
+    switch (foundEmployees.size()) {
+      case 0:
+        response = new NoMatch();
+        break;
+      case 1:
+        response = new PerfectMatch(foundEmployees.get(0));
+        break;
+      default:
+        response = new MultiplePartialMatch(foundEmployees);
     }
+    return response;
+  }
 
-    /**
-     * Detects if an {@link Employee} was requested and returns it
-     *
-     * @param optionIndex      The option index (1,2..) in an input {@link String}
-     * @param availableOptions A possible list of {@link NameValuePair} with the <code>id</code> of
-     *                         the employee in every {@link NameValuePair#getValue()}
-     * @return an {@link Optional} with the {@link Employee} correspondant to the <code>optionIndex</code>
-     */
-    protected Optional<Employee> getRequestedEmployee(String optionIndex,
-                                                      Optional<List<NameValuePair>> availableOptions) {
-        return availableOptions
-                .map(
-                        options -> options.stream().filter(pair -> pair.getName().equals(optionIndex))
-                                .findFirst()).map(this::getEmployeeFromOption).get();
-    }
+  /**
+   * Detects if an {@link Employee} was requested and returns it
+   *
+   * @param optionIndex The option index (1,2..) in an input {@link String}
+   * @param availableOptions A possible list of {@link NameValuePair} with the <code>id</code> of
+   *        the employee in every {@link NameValuePair#getValue()}
+   * @return an {@link Optional} with the {@link Employee} correspondant to the
+   *         <code>optionIndex</code>
+   */
+  protected Optional<Employee> getRequestedEmployee(String optionIndex,
+      Optional<List<NameValuePair>> availableOptions) {
+    return availableOptions
+        .map(
+            options -> options.stream().filter(pair -> pair.getName().equals(optionIndex))
+                .findFirst()).map(this::getEmployeeFromOption).orElse(Optional.empty());
+  }
 
-    /**
-     * Given some option returns the correspondant {@link Employee}
-     *
-     * @param choosenOption an {@link Optional} of {@link NameValuePair} linking the chosen option index with the wanted
-     *                      {@link Employee#id}
-     * @return an {@code Optional with the {@link Employee} found, not <code>null</code>
-     */
-    protected Optional<Employee> getEmployeeFromOption(Optional<NameValuePair> choosenOption) {
-        return Utils.getOptionalLong(choosenOption.get().getValue()).map(repository::findEmployeeById).get();
-    }
+  /**
+   * Given some option returns the correspondant {@link Employee}
+   *
+   * @param choosenOption an {@link Optional} of {@link NameValuePair} linking the chosen option
+   *        index with the wanted {@link Employee#id}
+   * @return an {@code Optional with the {@link Employee} found, not <code>null</code>
+   */
+  protected Optional<Employee> getEmployeeFromOption(Optional<NameValuePair> choosenOption) {
+    return choosenOption.map(option -> Utils.getOptionalLong(option.getValue())).map(
+        id -> repository.findEmployeeById(id.get()).get());
+  }
+
 }
